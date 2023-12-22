d788cd3f2a6b7a9c7a238d0f62163b38b4a0d30a
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-12-22 22:44:36 +0100

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,26 @@
-#![feature(gen_blocks, async_iterator)]
+#![feature(gen_blocks, async_iterator, coroutines, coroutine_trait)]
 
-pub gen fn test() -> &'static str {
-    yield "Hi";
+use std::{pin::Pin, ops::CoroutineState};
+use std::ops::Coroutine;
+
+pub fn take_iterator(iterator: impl Iterator<Item = i32>) {
+
+}
+
+pub fn test() {
+    let mut coroutine = |e: i32| {
+        yield e + 1;
+        yield e + 3;
+        "hi"
+    };
+
+    match Pin::new(&mut coroutine).resume(1) {
+        CoroutineState::Yielded(1) => {}
+        _ => panic!("unexpected value from resume"),
+    }
+
+    let iterator = gen {
+        yield 1;
+    };
+    take_iterator(iterator);
 }
